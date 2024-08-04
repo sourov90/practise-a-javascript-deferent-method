1ac42aa57e1e0a63ad10637aa6838149d7254e1f
sourov90 <sourovchandra921@gmail.com>
2024-08-04 10:18:38 +0600

    added some method and practise

---
+++ slice.js
@@ -0,0 +1,24 @@
+//example a slice method, if we call specefice method then use to slice.
+const names = 'my name is sree sourov chandra';
+// console.log(names.slice(11,22));
+
+//add and remove, then use to splice
+
+const numbers = [23,45,56,67,78,34];
+ numbers.splice(2,3);
+//  console.log(numbers)
+
+
+ //split means string to convert array...
+ const myNames='sree sourov chandra';
+//  console.log(myNames.split(' ').join(" "));
+
+ //concat... concate means do pair another variable
+ const firstName ='sourov';
+ const lastName =" " + 'chandra';
+ console.log(firstName.concat(lastName));
+
+ //toUpperCase and toLowerCase
+ const projects="each and everyday, i am practiceing english";
+ console.log(projects.toUpperCase())
+ console.log(projects.toLowerCase())
